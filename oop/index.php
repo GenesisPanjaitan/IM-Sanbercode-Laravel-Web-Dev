a1d1687c48e1d649d6b71cc4997f2ce91e5012e3
Genesis Tombak Dapot Tua Panjaitan <143679213+GenesisPanjaitan@users.noreply.github.com>
2025-08-14 20:43:46 +0700

    Tugas 9 – OOP PHP

---
+++ oop/index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once 'animal.php';
+    require_once 'frog.php';
+    require_once 'ape.php';
+
+    $sheep = new Animal("shaun");
+
+    echo "Name : " . $sheep->name . "<br>"; // "shaun"
+    echo "legs : " . $sheep->legs . "<br>"; // 4
+    echo "cold blooded : " . $sheep->cold_blooded . "<br><br>"; // "no"
+
+    $kodok = new Frog("buduk");
+    echo "Name : " . $kodok->name . "<br>";
+    echo "legs : " . $kodok->legs . "<br>";
+    echo "cold blooded : " . $kodok->cold_blooded . "<br>";
+    echo "Jump : " . $kodok->jump() . "<br><br>";
+
+    $sungokong = new Ape("kera sakti");
+    echo "Name : " . $sungokong->name . "<br>";
+    echo "legs : " . $sungokong->legs . "<br>";
+    echo "cold blooded : " . $sungokong->cold_blooded . "<br>";
+    echo "Yell : " . $sungokong->yell() . "<br>";
+?>
